3c5e0bac2af88cc5ef4466ce19f050d9d2c7b60a
Michel Morais <valentimmichel@gmail.com>
2023-02-26 17:57:25 -0300

    no-interface solution

---
+++ src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
+
+public class Program {
+	//Solução sem Interface
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		System.out.println("Enter rental details");
+		System.out.print("Car model: ");
+		String model = sc.nextLine();
+		System.out.print("Withdrawal (dd/MM/yyyy hh:mm): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.print("Return (dd/MM/yyyy hh:mm): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(model));
+		
+		System.out.print("Enter the price per hour: ");
+		double pricePerHour = sc.nextDouble();
+		System.out.print("Enter the price per day: ");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+		
+		rentalService.processInvoice(cr);
+		
+		System.out.println();
+		System.out.println("INVOICE:");
+		System.out.println("Basic payment: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("Tax: " + String.format("%.2f", cr.getInvoice().getTax()));
+		System.out.println("Total payment: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
+		
+		sc.close();
+	}
+
+}
